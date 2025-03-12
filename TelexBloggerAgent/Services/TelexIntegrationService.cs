3c37f518c209a97fd6568dd6bd714643c0b42659
Fabian Chibuike Muoghalu <fabbenco97@gmail.com>
2025-03-12 06:22:30 +0100

    Revert "[Chore]: Merge dev branch into main"

--- TelexBloggerAgent/Services/TelexIntegrationService.cs
+++ TelexBloggerAgent/Services/TelexIntegrationService.cs
@@ -6,21 +6,7 @@ namespace TelexBloggerAgent.Services
    
     public class TelexIntegrationService : ITelexIngegrationService
     {
-        private string _configurationFilePath = Path.Combine(Directory.GetCurrentDirectory(), "integration.json");
-
-
-        public string LoadIntegration()
-        {
-            if (!File.Exists(_configurationFilePath))
-            {
-                throw new FileNotFoundException("The configuration file was not found.");
-            }
-
-            var json = File.ReadAllText(_configurationFilePath);
-            using JsonDocument doc = JsonDocument.Parse(json);
-            return JsonSerializer.Serialize(doc.RootElement);
-
-        }
+        
     }
 }
    
