085bcb77fe3805ce4fd46aa639e3695da68ba9ac
Fabian Chibuike Muoghalu <fabbenco97@gmail.com>
2025-03-21 15:32:05 +0100

    feat: add models for the database storage

---
+++ TelexBloggerAgent/Models/Conversation.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using TelexBloggerAgent.IRepositories;
+
+namespace TelexBloggerAgent.Models
+{
+    public class Conversation : IEntity
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public string? CompanyId { get; set; } = null; // Links the conversation to a company
+
+        public string UserId { get; set; } = null!; // Who started the conversation
+
+        public List<Message> Messages { get; set; } = null!;
+
+        public string? ParentId { get; set; } // If it's a reply, it links to a parent conversation
+
+        //public List<string> Keywords { get; set; } = new(); // Keywords for context retrieval
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ TelexBloggerAgent/Models/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace TelexBloggerAgent.Models
+{
+    public class Message
+    {
+        public string Role { get; set; } = null!; // "user" or "ai"
+        public string Content { get; set; } = null!;
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
