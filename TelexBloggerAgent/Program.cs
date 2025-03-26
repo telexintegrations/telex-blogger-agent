66284669898095e6a9ebbf95b1fcef0634f7fd98
Adnanahmad <toyemubarak@gmail.com>
2025-03-26 13:53:08 +0100

    updt on prg
    update on program file

--- TelexBloggerAgent/Program.cs
+++ TelexBloggerAgent/Program.cs
@@ -9,8 +9,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 builder.Services.AddHttpClient();
 builder.Services.AddTransient<IBlogAgentService, BlogAgentService>();
 
